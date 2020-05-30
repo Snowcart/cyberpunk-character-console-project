562ee78717d79650639b217a4915111a7ece083f
Andrew Busch <andrew.busch7@gmail.com>
2020-05-30 16:43:44 +1000

    Adding local storage for character

--- app/src/hooks/useCharacter.tsx
+++ app/src/hooks/useCharacter.tsx
@@ -1,13 +1,16 @@
 import { useState, useCallback } from 'react';
 import { CharacterContext } from '../context/CharacterContext';
 import Character from '../models/character';
+import { getCharacterFromStorage, setCharacterInStorage } from '../services/characterRepository';
 
 export const useCharacter = (): CharacterContext => {
-	const [character, setCharacterObj] = useState({} as Character);
+	const characterFromStorage = getCharacterFromStorage();
+	const [character, setCharacterObj] = useState(characterFromStorage);
 	const [editable, setEdit] = useState(false);
 
 	const setCharacter = useCallback((character: Character): void => {
 		setCharacterObj(character);
+		setCharacterInStorage(character);
 	}, []);
 
 	const toggleEditable = useCallback((currentState: boolean) => {
