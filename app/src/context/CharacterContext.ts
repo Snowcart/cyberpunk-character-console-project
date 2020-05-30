e23ea5d060fb50a64ae342521b34e8354304efdf
Carter Snowden <snowcart@gmail.com>
2020-05-30 16:20:12 +1000

    Adding ability to edit

--- app/src/context/CharacterContext.ts
+++ app/src/context/CharacterContext.ts
@@ -1,14 +1,18 @@
-import Character from '../character';
+import Character from '../models/character';
 import * as React from 'react';
 
 export interface CharacterContext {
 	character: Character;
 	setCharacter: (character: Character) => void;
+	editable: boolean;
+	toggleEditable: (currentState: boolean) => void;
 }
 
 export const defaultCharacterContext = {
 	character: {} as Character,
-	setCharacter: () => {}
+	setCharacter: () => {},
+	editable: false,
+	toggleEditable: () => {}
 };
 
 export const characterContext = React.createContext<CharacterContext>(defaultCharacterContext);
--- app/src/title/Title.tsx
+++ app/src/title/Title.tsx
@@ -1,35 +1,41 @@
 import * as React from 'react';
-import { characterContext } from '../models/Context/CharacterContext';
+import { characterContext } from '../context/CharacterContext';
 import styled from 'styled-components';
 
 const Title = () => {
 	const characterCtx = React.useContext(characterContext);
 
-	const TitleBar = styled.div`
-		width: 100%;
-		height: 100px;
-		border-bottom: 2px solid #00ccff;
-		box-shadow: 2px 2px #00ffff;
-	`;
+	const onEditButtonClick = () => {
+		characterCtx.toggleEditable(characterCtx.editable);
+	};
 
-	const Button = styled.button`
-		float: right;
-	`;
+	const onHeadingValueChange = (e: any, name: string) => {
+		characterCtx.setCharacter({ ...characterCtx.character, [name]: e.target.value });
+	};
 
-	const Heading = styled.h1`
-		color: #00ccff;
-		text-shadow: 2px 1px #00ffff;
-		display: inline-block;
-		margin: auto 32px auto;
-		font-size: 64px;
-		height: 100%;
-		vertical-align: middle;
-	`;
+	const fields = (
+		<>
+			<Heading>HANDLE: </Heading>
+			<BoldHeading>{characterCtx.character.name}</BoldHeading>
+			<Heading>ROLE: </Heading>
+			<BoldHeading>{characterCtx.character.role}</BoldHeading>
+		</>
+	);
+
+	const editableFields = (
+		<>
+			<Heading>HANDLE: </Heading>
+			<Input value={characterCtx.character.name} onChange={(e) => onHeadingValueChange(e, 'name')} />
+			<Heading>ROLE: </Heading>
+			<Input value={characterCtx.character.role} onChange={(e) => onHeadingValueChange(e, 'role')} />
+		</>
+	);
 
 	const titleBar = (
 		<TitleBar>
 			<Heading> CCCP </Heading>
-			<Heading>{characterCtx.character.name}</Heading>
+			{characterCtx.editable ? editableFields : fields}
+			<Button onClick={() => onEditButtonClick()}>{characterCtx.editable ? 'Finish' : 'Edit'}</Button>
 			<Button>Import</Button>
 			<Button>Export</Button>
 		</TitleBar>
@@ -39,3 +45,45 @@ const Title = () => {
 };
 
 export default Title;
+
+const TitleBar = styled.div`
+	width: 100%;
+	height: 100px;
+	border-bottom: 2px solid #00ccff;
+	box-shadow: 2px 2px #00ffff;
+`;
+
+const Button = styled.button`
+	float: right;
+	cursor: pointer;
+`;
+
+const Heading = styled.h1`
+	color: #00ccff;
+	text-shadow: 2px 1px #00ffff;
+	display: inline-block;
+	margin-left: 40px;
+	height: 100%;
+	vertical-align: middle;
+	font-size: 42px;
+	font-weight: normal;
+	max-width: 25%;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+`;
+
+const BoldHeading = styled(Heading)`
+	font-weight: bold;
+	margin-left: 0;
+`;
+
+const Input = styled.input`
+	width: auto;
+	max-width: 25%;
+	font-size: 42px;
+	background-color: #2e2e2e;
+	border-color: #00ffff;
+	margin: auto;
+	color: #00ccff;
+`;
