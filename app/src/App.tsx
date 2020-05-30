4c0d3f661dfc2eda96c2da02f69047857592002d
Carter Snowden <snowcart@gmail.com>
2020-05-30 14:15:47 +1000

    Adding player context

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+import Character from './models/character';
 
 export default () => <App />;
 
 const App = () => {
+	const characterContext = {} as Character;
+	const CharacterContext = React.createContext(characterContext);
 	const GlobalStyle = createGlobalStyle`
 	body {
 		margin: 0;
-		background-color: #312c36;
+		background-color: #2e2e2e;
 	}
 	`;
 
