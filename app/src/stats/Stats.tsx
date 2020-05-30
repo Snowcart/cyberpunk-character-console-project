bd4db0360811f96aabce8a373879804e4961f7df
Andrew Busch <andrew.busch7@gmail.com>
2020-05-30 17:18:47 +1000

    Fixing character stats

--- app/src/stats/Stats.tsx
+++ app/src/stats/Stats.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 
 const Stats = () => {
 	const characterCtx = React.useContext(characterContext);
-	let stats: Stats = characterCtx.character ? characterCtx.character.stats : ({} as Stats);
+	let stats: Stats = characterCtx.character?.stats ?? ({} as Stats);
 
 	if (!stats.intelligence) {
 		stats.intelligence = 9;
