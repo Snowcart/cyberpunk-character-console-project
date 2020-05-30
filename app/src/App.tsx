f6b694eb4e49e561475c545275a2e28863671067
Andrew Busch <andrew.busch7@gmail.com>
2020-05-30 17:24:02 +1000

    Quick structure in App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,9 +5,7 @@ import { useCharacter } from './hooks/useCharacter';
 import { characterContext } from './context/CharacterContext';
 import Stats from './stats/Stats';
 
-export default () => <App />;
-
-const App = () => {
+export default () => {
 	const character = useCharacter();
 
 	const GlobalStyle = createGlobalStyle`
@@ -22,7 +20,16 @@ const App = () => {
 			<GlobalStyle />
 			<characterContext.Provider value={character}>
 				<Title />
-				<Stats />
+				<div>
+					<div>
+						<Stats />
+					</div>
+					<div>{/* Save / BTM */}</div>
+				</div>
+				<div>{/* Actions */}</div>
+				<div>{/* Inventory */}</div>
+				<div>{/* Cybernetics */}</div>
+				<div>{/* Stats */}</div>
 			</characterContext.Provider>
 		</>
 	);
