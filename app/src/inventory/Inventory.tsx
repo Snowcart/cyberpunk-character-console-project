c54fd434e3155b7fb2c5f14631b2350cb729c1e7
Carter Snowden <snowcart@gmail.com>
2020-06-07 14:16:26 +1000

    Removing unneeded code

--- app/src/inventory/Inventory.tsx
+++ app/src/inventory/Inventory.tsx
@@ -14,17 +14,3 @@ const Inventory = () => {
 };
 
 export default Inventory;
-
-const GearTitleWrapper = styled.div`
-	border-bottom: 1px solid blue;
-	width: 100%;
-	height: 15%;
-	min-height: 90px;
-	> h1 {
-		float: left;
-		max-width: 50%;
-		&:last-of-type {
-			float: left;
-		}
-	}
-`;
