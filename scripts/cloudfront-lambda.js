4f02664f3aed625b9800e8138ea129c7cc378aea
Andrew Busch <andrew.busch7@gmail.com>
2020-06-08 15:03:46 +1000

    Fixing cloudfront lambda, adding pipeline.yml

--- scripts/cloudfront-lambda.js
+++ scripts/cloudfront-lambda.js
@@ -1,36 +1,12 @@
-const path = require('path');
-const { STATUS_CODES } = require('http');
-
 exports.handler = async (event, context, callback) => {
   const { request } = event.Records[0].cf;
   const { uri } = request;
 
-  if (uri.indexOf('index') !== -1) {
-    return callback(null, redirect('/'));
-  }
-
   const htmlExtRegex = /(.*)\.html?$/;
-  if (htmlExtRegex.test(request.uri)) {
-    const uri = request.uri.replace(htmlExtRegex, '$1');
-    return callback(null, redirect(uri));
+  const periodExtRegex = /\./;
+  if (htmlExtRegex.test(request.uri) || !periodExtRegex.test(request.uri)) {
+    request.uri = '/index.html';
   }
-  if (!uri || uri === '/') {
-    request.uri = 'index.html';
-  } else if (uri.indexOf('.') === -1) {
-    request.uri = uri + '.html';
-  }
-
-  // callback(null, staticContent('URI: [' + request.uri + ']'));
 
   callback(null, request);
 };
-
-function redirect(to) {
-  return {
-    status: '301',
-    statusDescription: STATUS_CODES['301'],
-    headers: {
-      location: [{ key: 'Location', value: to }],
-    },
-  };
-}
